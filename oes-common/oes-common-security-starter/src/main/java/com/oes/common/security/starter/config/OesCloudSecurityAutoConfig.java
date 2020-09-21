a5efee9f3c05062fa40694f23df3d3e373339239
chachae <chenyuexin1998@gmail.com>
2020-09-21 13:31:38 +0800

    fix: 指定资源id下无法访问目标资源的问题

--- oes-common/oes-common-security-starter/src/main/java/com/oes/common/security/starter/config/OesCloudSecurityAutoConfig.java
+++ oes-common/oes-common-security-starter/src/main/java/com/oes/common/security/starter/config/OesCloudSecurityAutoConfig.java
@@ -9,12 +9,18 @@ import com.oes.common.security.starter.properties.OesCloudSecurityProperties;
 import feign.RequestInterceptor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.http.HttpHeaders;
+import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.provider.expression.OAuth2MethodSecurityExpressionHandler;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.util.Base64Utils;
 
 /**
@@ -23,7 +29,7 @@ import org.springframework.util.Base64Utils;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableConfigurationProperties(OesCloudSecurityProperties.class)
 @ConditionalOnProperty(value = "oes.cloud.security.enable", havingValue = "true", matchIfMissing = true)
-public class OesCloudSecurityAutoConfig {
+public class OesCloudSecurityAutoConfig extends GlobalMethodSecurityConfiguration {
 
   @Bean
   @ConditionalOnMissingBean(name = "accessDeniedHandler")
@@ -44,8 +50,21 @@ public class OesCloudSecurityAutoConfig {
   }
 
   @Bean
-  public OesCloudSecurityInterceptorConfig oesCloudSecurityInterceptorConfigure() {
+  public OesCloudSecurityInterceptorConfig oesCloudSecurityInterceptorConfig() {
     return new OesCloudSecurityInterceptorConfig();
+  }
+
+  @Bean
+  @Primary
+  @ConditionalOnMissingBean(DefaultTokenServices.class)
+  public OesUserInfoTokenServices oesUserInfoTokenServices(ResourceServerProperties properties) {
+    return new OesUserInfoTokenServices(properties.getUserInfoUri(), properties.getClientId());
+  }
+
+
+  @Override
+  protected MethodSecurityExpressionHandler createExpressionHandler() {
+    return new OAuth2MethodSecurityExpressionHandler();
   }
 
   /**
@@ -59,8 +78,7 @@ public class OesCloudSecurityAutoConfig {
       String gatewayToken = new String(Base64Utils.encode(GatewayConstant.TOKEN_VALUE.getBytes()));
       requestTemplate
           .header(GatewayConstant.TOKEN_HEADER, gatewayToken)
-          .header(HttpHeaders.AUTHORIZATION,
-              SystemConstant.OAUTH2_TOKEN_TYPE + SecurityUtil.getCurrentTokenValue());
+          .header(HttpHeaders.AUTHORIZATION, SystemConstant.OAUTH2_TOKEN_TYPE + SecurityUtil.getCurrentTokenValue());
     };
   }
 }
