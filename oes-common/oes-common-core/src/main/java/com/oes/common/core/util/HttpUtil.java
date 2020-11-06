19205107db6d74e5f8a521ec041799ee51ce2765
chachae <chenyuexin1998@gmail.com>
2020-11-06 10:20:40 +0800

    fix: 修复IP方法多级代理下无法获取真实地址的问题

--- oes-common/oes-common-core/src/main/java/com/oes/common/core/util/HttpUtil.java
+++ oes-common/oes-common-core/src/main/java/com/oes/common/core/util/HttpUtil.java
@@ -1,9 +1,7 @@
 package com.oes.common.core.util;
 
-import cn.hutool.core.net.NetUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.http.useragent.UserAgentUtil;
-import com.google.common.net.HttpHeaders;
 import com.oes.common.core.entity.UserAgent;
 import java.io.IOException;
 import java.util.Objects;
@@ -11,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.server.reactive.ServerHttpRequest;
@@ -32,7 +31,7 @@ public class HttpUtil extends cn.hutool.http.HttpUtil {
   }
 
 
-  private static final String[] LOCALHOST = {"unknown", "127.0.0.1", "0:0:0:0:0:0:0:1"};
+  private static final String UNKNOWN = "unknown";
 
   /**
    * 设置响应
@@ -161,21 +160,19 @@ public class HttpUtil extends cn.hutool.http.HttpUtil {
    */
   public static String getIpAddress() {
     HttpServletRequest request = getHttpServletRequest();
-    String ip = request.getHeader(HttpHeaders.X_FORWARDED_FOR);
-    if (ip == null || ip.length() == 0 || LOCALHOST[0].equalsIgnoreCase(ip)) {
+    String ip = request.getHeader("x-forwarded-for");
+    if (ip == null || ip.length() == 0 || UNKNOWN.equalsIgnoreCase(ip)) {
       ip = request.getHeader("Proxy-Client-IP");
     }
-    if (ip == null || ip.length() == 0 || LOCALHOST[0].equalsIgnoreCase(ip)) {
+    if (ip == null || ip.length() == 0 || UNKNOWN.equalsIgnoreCase(ip)) {
       ip = request.getHeader("WL-Proxy-Client-IP");
     }
-    if (ip == null || ip.length() == 0 || LOCALHOST[0].equalsIgnoreCase(ip)) {
+    if (ip == null || ip.length() == 0 || UNKNOWN.equalsIgnoreCase(ip)) {
       ip = request.getRemoteAddr();
     }
-    if (LOCALHOST[0].equalsIgnoreCase(ip) || LOCALHOST[1].equalsIgnoreCase(ip) || LOCALHOST[2]
-        .equalsIgnoreCase(ip)) {
-      ip = NetUtil.getLocalhostStr();
-    }
-    return ip;
+    // 处理一下多级代理
+    ip = StrUtil.subBefore(ip, ',', false);
+    return "0:0:0:0:0:0:0:1".equals(ip) ? "127.0.0.1" : ip;
   }
 
   /**
@@ -187,30 +184,31 @@ public class HttpUtil extends cn.hutool.http.HttpUtil {
   public static String getIpAddress(ServerHttpRequest request) {
     org.springframework.http.HttpHeaders headers = request.getHeaders();
     String ip = headers.getFirst("x-forwarded-for");
-    if (ip != null && ip.length() != 0 && !LOCALHOST[0].equalsIgnoreCase(ip)) {
+    if (ip != null && ip.length() != 0 && !UNKNOWN.equalsIgnoreCase(ip)) {
       if (ip.contains(StrUtil.COMMA)) {
         ip = ip.split(StrUtil.COMMA)[0];
       }
     }
-    if (ip == null || ip.length() == 0 || LOCALHOST[0].equalsIgnoreCase(ip)) {
+    if (ip == null || ip.length() == 0 || UNKNOWN.equalsIgnoreCase(ip)) {
       ip = headers.getFirst("Proxy-Client-IP");
     }
-    if (ip == null || ip.length() == 0 || LOCALHOST[0].equalsIgnoreCase(ip)) {
+    if (ip == null || ip.length() == 0 || UNKNOWN.equalsIgnoreCase(ip)) {
       ip = headers.getFirst("WL-Proxy-Client-IP");
     }
-    if (ip == null || ip.length() == 0 || LOCALHOST[0].equalsIgnoreCase(ip)) {
+    if (ip == null || ip.length() == 0 || UNKNOWN.equalsIgnoreCase(ip)) {
       ip = headers.getFirst("HTTP_CLIENT_IP");
     }
-    if (ip == null || ip.length() == 0 || LOCALHOST[0].equalsIgnoreCase(ip)) {
+    if (ip == null || ip.length() == 0 || UNKNOWN.equalsIgnoreCase(ip)) {
       ip = headers.getFirst("HTTP_X_FORWARDED_FOR");
     }
-    if (ip == null || ip.length() == 0 || LOCALHOST[0].equalsIgnoreCase(ip)) {
+    if (ip == null || ip.length() == 0 || UNKNOWN.equalsIgnoreCase(ip)) {
       ip = headers.getFirst("X-Real-IP");
     }
-    if (ip == null || ip.length() == 0 || LOCALHOST[0].equalsIgnoreCase(ip)) {
+    if (ip == null || ip.length() == 0 || UNKNOWN.equalsIgnoreCase(ip)) {
       ip = Objects.requireNonNull(request.getRemoteAddress()).getAddress().getHostAddress();
     }
-    return LOCALHOST[2].equals(ip) ? LOCALHOST[1] : ip;
+    ip = StrUtil.subBefore(ip, ',', false);
+    return "0:0:0:0:0:0:0:1".equals(ip) ? "127.0.0.1" : ip;
   }
 
   /**
